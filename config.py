db3056c3d83620407941880d266e55a496ae9f0a
sunta614 <sunitasonawale614@gmail.com>
2025-04-23 13:16:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("7130569401:AAGx9zEZAt6py6TpD9D83TZh2cHH5G1eYkM")
     API_ID = int(os.environ.get("22923037"))
     API_HASH = os.environ.get("dfb3666878b3851460a58461c5a50f5b")
-    AUTH_USERS = os.environ.get("6554343173")
+    AUTH_USERS = os.environ.get("AUTH_USERS")
